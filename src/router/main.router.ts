c9f7523d66c04858e8030ce8872f3d00608a0e87
Saviour Philip <ukosaviour21@gmail.com>
2024-02-06 22:01:13 +0100

    customer priviledge added

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -14,11 +14,11 @@ main_router.post('/user/Signup', SignUp);
 
 main_router.post('/user/Login', LogInUser);
 
-main_router.get('/user/findOne/:id', ensureAuth, restrict([UserType.ADMIN]), FindOneUser );
+main_router.get('/user/findOne/:id', ensureAuth, restrict([UserType.ADMIN, UserType.CUSTOMER]), FindOneUser );
 main_router.get('/user/findAll', FindAllUser );
 // main_router.get('/user/findAll', ensureAuth, restrict([UserType.ADMIN]), FindAllUser );
 
-main_router.post('/upload/stock', ensureAuth, restrict([UserType.ADMIN]), multerUpload.single('file'), UploadStock);
+main_router.post('/upload/stock', ensureAuth, restrict([UserType.ADMIN, UserType.CUSTOMER]), multerUpload.single('file'), UploadStock);
 
 main_router.get('/user/logout', ensureAuth, LogoutUser );
 
@@ -38,11 +38,11 @@ main_router.get('/stock/findOne/:id', GetOneStockById);
 
 main_router.get('/stock/findItemsByName/:name', FindStocksByName);
 
-main_router.delete('/delete/stockById/:id', ensureAuth, restrict([UserType.ADMIN, UserType.MODERATOR]), DeleteStockById);
+main_router.delete('/delete/stockById/:id', ensureAuth, restrict([UserType.ADMIN, UserType.CUSTOMER, UserType.MODERATOR]), DeleteStockById);
 
-main_router.put('/user/suspend/:id', ensureAuth, restrict([UserType.ADMIN, UserType.MODERATOR]), SuspendUser);
+main_router.put('/user/suspend/:id', ensureAuth, restrict([UserType.CUSTOMER, UserType.ADMIN, UserType.MODERATOR]), SuspendUser);
 
-main_router.put('/user/unSuspend/:id', ensureAuth, restrict([UserType.ADMIN, UserType.MODERATOR]), UnSuspendUser);
+main_router.put('/user/unSuspend/:id', ensureAuth, restrict([UserType.CUSTOMER, UserType.ADMIN, UserType.MODERATOR]), UnSuspendUser);
 
 main_router.put('/user/editProfile/:id', ensureAuth, EditProfile);
 
