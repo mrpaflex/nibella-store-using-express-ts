ad7af99e9adfc47ab3e6dc78621e947440106f9a
Saviour Philip <ukosaviour21@gmail.com>
2024-02-19 01:25:26 +0100

    auth remove on add to cart

--- src/router/main.router.ts
+++ src/router/main.router.ts
@@ -57,7 +57,7 @@ main_router.put('/user/editProfile/:id', ensureAuth, EditProfile);
 
 main_router.get('/user/delete/:username', DeleteUser );
 
-main_router.post('/user/addToCart/:id',ensureAuth, AddToCart);
+main_router.post('/user/addToCart/:id', AddToCart);
 
 main_router.get('/confirmed/orderPage', ensureAuth, CheckoutOrder);
 main_router.post('/stock/payment', ensureAuth, StocksPayment);
