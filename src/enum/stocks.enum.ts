edc273e4eabb1d0c9f0cce1f0ff8b201b4362aea
Saviour Philip <ukosaviour21@gmail.com>
2024-02-23 05:33:22 +0100

    male to male

--- src/enum/stocks.enum.ts
+++ src/enum/stocks.enum.ts
@@ -1,6 +1,6 @@
 export enum StockTypes{
     "HAIR"= "Hair",
-    "MEN_CLOTHES"= "Male",
+    "MEN_CLOTHES"= "Men",
     "SHOES"= "Shoes",
     "COMESTICS"= "Cosmetics",
     "WOMEN_CLOTHES"="Women"
